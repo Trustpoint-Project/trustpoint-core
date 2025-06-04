bb0a2472584c92ae42a0363a385184389c0a5adb
AlexHx8472 <alex_mail@posteo.de>
2025-06-04 11:57:00 +0200

    ruff mypy fixes

--- trustpoint_core/crypto_types.py
+++ trustpoint_core/crypto_types.py
@@ -7,5 +7,13 @@ from cryptography.hazmat.primitives.asymmetric import ec, ed448, ed25519, rsa
 
 PublicKey = rsa.RSAPublicKey | ec.EllipticCurvePublicKey | ed25519.Ed25519PublicKey | ed448.Ed448PublicKey
 PrivateKey = rsa.RSAPrivateKey | ec.EllipticCurvePrivateKey | ed25519.Ed25519PrivateKey | ed448.Ed448PrivateKey
-AllowedCertSignHashAlgos = hashes.SHA224 | hashes.SHA256 | hashes.SHA384 | hashes.SHA512 | hashes.SHA3_224 | hashes.SHA3_256 | hashes.SHA3_384 | hashes.SHA3_512,
-
+AllowedCertSignHashAlgos = (
+    hashes.SHA224
+    | hashes.SHA256
+    | hashes.SHA384
+    | hashes.SHA512
+    | hashes.SHA3_224
+    | hashes.SHA3_256
+    | hashes.SHA3_384
+    | hashes.SHA3_512
+)
